92c3320ca6e7c5b6aaf77ff71b4750446a836f83
Ruth Cedeño <emperatrizcedeno18@gmail.com>
2023-04-26 20:13:51 -0400

    Mapeo de la Base de Datos, donde se añadió las clases de las entidades con sus relaciones

---
+++ Domain/Entities/Estudiante.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Domain.Entities
+{
+	public class Estudiante
+	{
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Correo { get; set; }
+        public string Direccion { get; set; }
+        public int CarreraID { get; set; }
+        public int TutorID { get; set; }
+        public int ServicioID { get; set; }
+        public int UserID { get; set; }
+        public string Telefono { get; set; }
+        public Carrera Carrera { get; set; }
+        public Tutor Tutor { get; set; }
+        public Servicio Servicio { get; set; }
+        public Usuario Usuario { get; set; }
+    }
+}
---
+++ Domain/Entities/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Domain.Entities
+{
+	public class Usuario
+	{
+        public int Id { get; set; }
+        public string Cedula { get; set; }
+        public int RolID { get; set; }
+        public string Clave { get; set; }
+        public Administrador Administrador { get; set; }
+        public Estudiante Estudiante { get; set; }
+        public Rol Rol { get; set; }
+    }
+}
