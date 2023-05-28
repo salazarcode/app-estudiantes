d67b1c5556266f04bdbb79c2a3f4f2f19589a114
Andrés Salazar <c-eduardo.salazar@poppulo.com>
2023-05-28 20:03:05 -0300

    Usuarios PUT modificado

--- API/Controllers/UsuarioController.cs
+++ API/Controllers/UsuarioController.cs
@@ -1,28 +1,30 @@
-using API.AuthFilter;
 using API.DTO.Requests.Usuario;
 using AutoMapper;
 using Domain.Entities;
 using Domain.Interfaces.Services;
+using Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
 	[ApiController]
 	[Route("[controller]")]
-//	[AuthenticationFilter("administrador")]
+	//	[AuthenticationFilter("administrador")]
 	public class UsuarioController : ControllerBase
 	{
 		private readonly IUsuarioService _usuariosService;
 		private readonly IEstudianteService _estudianteService;
 		private readonly IServicioService _servicioService;
+		private readonly IEncrypter	_encrypter;
 		private readonly IMapper _mapper;
 
-		public UsuarioController(IUsuarioService usuariosService, IMapper mapper, IEstudianteService estudianteService, IServicioService servicioService)
+		public UsuarioController(IUsuarioService usuariosService, IMapper mapper, IEstudianteService estudianteService, IServicioService servicioService, IEncrypter encrypter)
 		{
 			_usuariosService = usuariosService;
 			_mapper = mapper;
 			_estudianteService = estudianteService;
 			_servicioService = servicioService;
+			_encrypter = encrypter;
 		}
 
 		[HttpGet]
@@ -34,9 +36,23 @@ namespace API.Controllers
 
 
 		[HttpPut]
-		public IActionResult Update([FromBody] UpdateUsuarioDTO input)
+		[Route("{UsuarioID}")]
+		public IActionResult Update([FromRoute] int UsuarioID, [FromBody] UpdateUsuarioDTO input)
 		{
-			var usuario = _mapper.Map<Usuario>(input);
+			var usuario = _usuariosService.Get(UsuarioID).FirstOrDefault();
+			//valiadacion de null
+			if (usuario == null)
+				return BadRequest("Usuario no encontrado");
+
+			usuario.Nombre = input.Nombre ?? usuario.Nombre;
+			usuario.Apellido = input.Apellido ?? usuario.Apellido;
+			usuario.Cedula = input.Cedula ?? usuario.Cedula;
+			usuario.Clave = input.Clave != null ? _encrypter.Encrypt(input.Clave) : usuario.Clave;
+			usuario.RoleId = input.RoleId ?? usuario.RoleId;
+			usuario.Direccion	= input.Direccion ?? usuario.Direccion;
+			usuario.Telefono	= input.Telefono ?? usuario.Telefono;
+			usuario.Correo		= input.Correo ?? usuario.Correo;
+
 			var res = _usuariosService.Update(usuario);
 			return Ok(res);
 		}
@@ -49,8 +65,9 @@ namespace API.Controllers
 
 			if (usuario == null)
 				return BadRequest("Usuario no encontrado");
-			
-			if (usuario.Role.Nombre.ToLower() == "estudiante") { 				
+
+			if (usuario.Role.Nombre.ToLower() == "estudiante")
+			{
 				var estudiante = _estudianteService.GetAll().FirstOrDefault(x => x.UserId == usuario.Id);
 
 				if (estudiante != null)
@@ -58,7 +75,7 @@ namespace API.Controllers
 					var servicio = _servicioService.GetAll().FirstOrDefault(x => x.Id == estudiante.ServicioId);
 					_servicioService.Remove(servicio);
 
-					_estudianteService.Remove(estudiante);	
+					_estudianteService.Remove(estudiante);
 				}
 			}
 
--- API/DTO/Requests/Usuario/UpdateUsuarioDTO.cs
+++ API/DTO/Requests/Usuario/UpdateUsuarioDTO.cs
@@ -2,9 +2,20 @@
 {
     public class UpdateUsuarioDTO
     {
-        public int? id { get; set; }
-        public string? Cedula { get; set; }
-        public int? RoleId { get; set; }
-        public string? Clave { get; set; }
-    }
+		public string? Cedula { get; set; }
+
+		public int? RoleId { get; set; }
+
+		public string? Clave { get; set; }
+
+		public string? Nombre { get; set; }
+
+		public string? Apellido { get; set; }
+
+		public string? Correo { get; set; }
+
+		public string? Telefono { get; set; }
+
+		public string? Direccion { get; set; }
+	}
 }
